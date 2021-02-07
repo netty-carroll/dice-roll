6425b6d2de5698a29f4bedff5a86f38436e1277e
netty <netty.carroll@live.com>
2021-02-07 20:25:10 +0000

    js
    attemted roll

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const dice = document.querySelector('#dice');
+const roll = document.querySelector('button');
+const rollDie = ()=> Math.floor(math.random()*6 + 1);
+const rollDice = ()=> `img src="dice${rollDie()}.png">`
 
+roll.onclick = ()=>dice.innerHTML = rollDice();
